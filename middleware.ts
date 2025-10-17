07a1ed8a00018342505f6a68dcd95421368d40a6
guhl <89780012@qq.com>
2025-10-17 21:41:30 +0800

    optimize: 增加谷歌sgc

--- middleware.ts
+++ middleware.ts
@@ -22,6 +22,6 @@ export const config = {
     // - _next/static (static files)
     // - _next/image (image optimization files)
     // - favicon.ico (favicon file)
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|google58a1ae2292da82f2.html).*)',
   ]
 };
